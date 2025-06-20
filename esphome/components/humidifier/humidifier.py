6de2d6a75b33943d6a6e560b08d655bdf1d253ca
plplaaa2 <124797654+plplaaa2@users.noreply.github.com>
2025-06-20 14:52:51 +0900

    Update humidifier.py

--- esphome/components/humidifier/humidifier.py
+++ esphome/components/humidifier/humidifier.py
@@ -1,8 +1,4 @@
 import esphome.codegen as cg
-
-humidifier_ns = cg.esphome_ns.namespace("humidifier")
-Humidifier = humidifier_ns.class_("Humidifier", cg.Component)
-
 import esphome.config_validation as cv
 from esphome.components import climate, sensor
 from esphome.const import (
@@ -11,16 +7,20 @@ from esphome.const import (
     CONF_SENSOR,
 )
 
-CONF_FAN_MODES = "fan_modes"
+# 고정 팬모드 목록
+DEFAULT_FAN_MODES = ["OFF", "LOW", "MEDIUM", "HIGH"]
+
 CONF_MIN_HUMIDITY = "min_humidity"
 CONF_MAX_HUMIDITY = "max_humidity"
+
+humidifier_ns = cg.esphome_ns.namespace("humidifier")
+Humidifier = humidifier_ns.class_("Humidifier", cg.Component)
 
 CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(Humidifier),
         cv.Required(CONF_NAME): cv.string,
         cv.Required(CONF_SENSOR): cv.use_id(sensor.Sensor),
-        cv.Optional(CONF_FAN_MODES, default=["OFF", "LOW", "MEDIUM", "HIGH"]): cv.ensure_list(cv.string),
         cv.Optional(CONF_MIN_HUMIDITY, default=30): cv.int_range(min=0, max=100),
         cv.Optional(CONF_MAX_HUMIDITY, default=80): cv.int_range(min=0, max=100),
     }
@@ -31,7 +31,7 @@ def to_code(config):
     yield climate.register_climate(var, config)
     sens = yield cg.get_variable(config[CONF_SENSOR])
     cg.add(var.set_humidity_sensor(sens))
-    cg.add(var.set_supported_fan_modes(config[CONF_FAN_MODES]))
+    cg.add(var.set_supported_fan_modes(DEFAULT_FAN_MODES))  # 팬모드 고정
     cg.add(var.set_min_humidity(config[CONF_MIN_HUMIDITY]))
     cg.add(var.set_max_humidity(config[CONF_MAX_HUMIDITY]))
     yield cg.register_component(var, config)
