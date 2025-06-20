9f62b1201045f5231e459665296220bbb989b2e3
plplaaa2 <124797654+plplaaa2@users.noreply.github.com>
2025-06-20 14:46:05 +0900

    Update __init__.py

--- esphome/components/humidifier/__init__.py
+++ esphome/components/humidifier/__init__.py
@@ -2,6 +2,3 @@ import esphome.codegen as cg
 from .humidifier import CONFIG_SCHEMA, to_code
 
 CODEOWNERS = ["@plplaaa2"]
-
-humidifier_ns = cg.esphome_ns.namespace("humidifier")
-Humidifier = humidifier_ns.class_("Humidifier", cg.Component)
