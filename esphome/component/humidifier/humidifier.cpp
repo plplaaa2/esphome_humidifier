dc3f242a8a43cbce0d7bd18b000330703537c0b0
plplaaa2 <124797654+plplaaa2@users.noreply.github.com>
2025-06-20 14:13:57 +0900

    Update humidifier.cpp

--- esphome/component/humidifier/humidifier.cpp
+++ esphome/component/humidifier/humidifier.cpp
@@ -39,6 +39,8 @@ climate::ClimateTraits Humidifier::traits() {
   traits.set_supports_target_humidity(true);
   traits.set_supported_fan_modes(this->fan_modes_);
   traits.set_supported_modes({climate::CLIMATE_MODE_OFF, climate::CLIMATE_MODE_AUTO});
+  traits.set_min_humidity(30);
+  traits.set_max_humidity(80);
   return traits;
 }
 
