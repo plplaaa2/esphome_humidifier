4f9f290bca49a260b39f52273928c322d56d0253
plplaaa2 <124797654+plplaaa2@users.noreply.github.com>
2025-06-20 14:06:22 +0900

    Update sensor.py

--- esphome/component/humidifier/sensor.py
+++ esphome/component/humidifier/sensor.py
@@ -1,1 +1,19 @@
 
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome.const import CONF_ID, CONF_SENSOR
+
+from . import humidifier_ns, Humidifier
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.use_id(Humidifier),
+        cv.Required(CONF_SENSOR): cv.use_id(sensor.Sensor),
+    }
+)
+
+def to_code(config):
+    par = yield cg.get_variable(config[CONF_ID])
+    sens = yield cg.get_variable(config[CONF_SENSOR])
+    cg.add(par.set_humidity_sensor(sens))
